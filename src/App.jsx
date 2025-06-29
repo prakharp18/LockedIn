1fca233549357977b11df6c42a87dca537e08c8f
prakharp18 <pporwal2019@gmail.com>
2025-06-29 12:54:00 +0530

    initial commit with vite+react+tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black dark:bg-[#121212] dark:text-white transition-colors duration-300">
+      {/* Components will go here */}
+      <h1 className="text-3xl font-bold text-center mt-10">LockedIn</h1>
+    </div>
+  );
+}
+
+export default App;
